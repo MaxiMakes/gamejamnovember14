f7d9be9b2e2eedd8893395abe9b6578766af78a0
veryrandomname <seriousemailadress@googlemail.com>
2014-11-22 15:50:00 +0100

    control points

---
+++ controlpoint.lua
@@ -0,0 +1,90 @@
+local control = {}
+control.mt = { __index = control }
+
+local takeoverSpeed = 10
+
+local function defaultF(self)
+  self.storage = self.storage + 10 
+  return self.storage 
+end
+
+function control.new(x,y,dx,dy,f,interval)
+  local newPoint = {}
+  setmetatable(newPoint,control.mt)
+
+  newPoint.storage = 0
+  newPoint.t = 0
+  newPoint.f = f or defaultF
+  newPoint.interval = interval or 5
+  newPoint.playersAround = {}
+  newPoint.ownership = 0
+
+  newPoint.body = love.physics.newBody(world, x+dx/2, y+dy/2) 
+  newPoint.shape = love.physics.newRectangleShape(dx, dy)
+  newPoint.fixture = love.physics.newFixture(newPoint.body, newPoint.shape) --attach shape to body
+  newPoint.fixture:setSensor(true)
+
+  return newPoint
+end
+
+function control:update(dt)
+  --restock storage
+  self.t = self.t + dt
+
+  if self.t > self.interval then
+    self.t = self.t - self.interval
+
+    self:f()
+  end
+
+  --react to players around
+  if #self.playersAround == 1 then
+    local p = self.playersAround[1]
+    if p == self.owner then
+      player.storage = self.storage
+      self.storage = 0
+    elseif p == self.newGuy then
+      self.ownership = self.ownership + takeoverSpeed
+      if self.ownership > 0 then
+        self.owner = p
+      end
+    else
+      self.newGuy = player
+      self.ownership = takeoverSpeed
+    end
+  end
+end
+
+function control:draw()
+  love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+  love.graphics.print('Hello World!', self.body:getX()+10, self.body:getY()+10)
+  love.graphics.print(self.storage, self.body:getX()+10, self.body:getY()+30)
+  if self.owner then
+    love.graphics.print('owner' .. self.owner, self.body:getX()+10, self.body:getY()+50)
+  end
+  love.graphics.print('spieler ' .. #self.playersAround, self.body:getX()+10, self.body:getY()+70)
+end
+
+function control:ownedBy(player)
+  self.owner = player
+end
+
+function control.beginContact(self, b, coll)
+  for i,v in ipairs(self.playersAround) do
+    if v == b then
+      return
+    end
+  end
+
+  table.insert(self.playersAround,b)
+end
+
+function control.endContact(self, b, coll)
+  for i,v in ipairs(self.playersAround) do
+    if v == b then
+      table.remove(self.playersAround,i)
+    end
+  end
+end
+
+return control
--- main.lua
+++ main.lua
@@ -1,13 +1,14 @@
 local player = require 'player'
+local control = require ("controlpoint")
 
 function love.load()
   love.physics.setMeter(64) --the height of a meter our worlds will be 64px
-  world = love.physics.newWorld(0, 0, true) --create a world for the bodies to exist in with
+  world = love.physics.newWorld(0, 0, true) --create a world for the bodies to exist in with 
+  world:setCallbacks(beginContact, endContact, preSolve, postSolve)
+
   --creating player
   player.new("player1", 100, 100)
   player.load()
-
-
 
   objects = {} -- table to hold all our physical objects
 
@@ -19,11 +20,19 @@ function love.load()
   --objects.ball.fixture:setRestitution(0.9) --let the ball bounce
 
   love.graphics.setBackgroundColor(104, 136, 248) --set the background color to a nice blue
-  love.window.setMode(650, 650) --set the window dimensions to 650 by 650 with no fullscreen, vsync on,
+  love.window.setMode(650, 650) --set the window dimensions to 650 by 650 with no fullscreen, vsync on, 
+
+  -- test the control points 
+  controlPoints = {}
+  table.insert(controlPoints, control.new(650/2,650/2,10,10))
 end
 
 function love.update(dt)
   world:update(dt) --this puts the world into motion
+
+  for i,v in ipairs(controlPoints) do
+    v:update(dt)
+  end
 end
 
 function love.keypressed(key)
@@ -38,4 +47,42 @@ function love.draw()
   player.draw()
   love.graphics.print('Hello World!', 400, 300)
   love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+
+  for i,v in ipairs(controlPoints) do
+    v:draw()
+  end
+end
+
+function love.keypressed(key)
+   if key == "escape" then
+      love.event.quit()
+   end
+end
+
+function beginContact(a, b, coll)
+  for i,v in ipairs(controlPoints) do
+    if a == v.fixture then
+      v.beginContact(v, b, coll)
+    elseif b == v.fixture then
+      v.beginContact(v, a, coll)
+    end
+  end
+end
+
+function endContact(a, b, coll)
+   for i,v in ipairs(controlPoints) do
+    if a == v.fixture then
+      v.endContact(v, b, coll)
+    elseif b == v.fixture then
+      v.endContact(v, a, coll)
+    end
+  end
+end
+
+function preSolve(a, b, coll)
+   
+end
+
+function postSolve(a, b, coll, normalimpulse1, tangentimpulse1, normalimpulse2, tangentimpulse2)
+   
 end
